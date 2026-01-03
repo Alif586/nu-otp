8916a8213027ae4ce40be8c08ec52b92eedf1390
Alif586 <151447720+Alif586@users.noreply.github.com>
2026-01-03 16:23:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const PORT = 3000;
 
-// Public folder serve
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Front-end JS serve from /js folder
-/*app.get('/app.js', (req, res) => {
-  res.sendFile(path.join(__dirname, 'js', 'app.js'));
-});*/
-
-// Root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = app;
