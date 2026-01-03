788efa777dcc2bd63072428329666f910450cf1b
Alif586 <151447720+Alif586@users.noreply.github.com>
2026-01-03 15:57:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
+// Public folder serve
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Front-end JS serve from /js folder
+/*app.get('/app.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'js', 'app.js'));
+});*/
+
+// Root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
